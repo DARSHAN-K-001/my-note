e69dd09ca0e19aae0b59a56ccf2996fc68071c89
DARSHAN-K-001 <99954341+DARSHAN-K-001@users.noreply.github.com>
2022-03-12 22:53:45 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>my notes <applet></applet></h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
